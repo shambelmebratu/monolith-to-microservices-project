21c710191be4574b137f4b46e87ab1bfb7f0a34c
shambi21 <shambi2008@gmail.com>
2022-09-28 09:49:33 +0300

    commit my project 3

--- set_env.sh
+++ set_env.sh
@@ -1,11 +1,11 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=mypassword
-export POSTGRES_HOST=postgres.cr9bldgsf1j6.us-east-1.rds.amazonaws.com
+export POSTGRES_USERNAME=shambi21
+export POSTGRES_PASSWORD=shambi21
+export POSTGRES_HOST=project3-shambel.cmvytcjktksx.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=postgres
-export AWS_BUCKET=arn:aws:s3:::mybucket1200798
+export AWS_BUCKET=project3-693574642211
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
-export JWT_SECRET=testing
+export JWT_SECRET=SM-dev
 export URL=http://localhost:8100
